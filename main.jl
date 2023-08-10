d659bd3446143789f95af0ecd6dca7422c2d403f
FrancisBanville <francis.banville@umontreal.ca>
2023-08-10 17:27:57 -0400

    :art: add main script and packages

---
+++ main.jl
@@ -0,0 +1,32 @@
+## Activate project environment
+## Creates a list of packages and dependencies with specific versions used
+## for reproducibility reasons
+## See Project.toml and Manifest.toml files
+import Pkg; Pkg.activate(".")
+
+Pkg.instantiate()
+
+## Load required packages
+## Use Pkg.add("Package Name") to add other packages
+
+# Manipulating variables, data frames and files
+import CSV 
+using DataFrames 
+using JLD2
+
+# Doing statistics and models
+using Random
+using Statistics
+using StatsBase
+
+# Making plots
+using Plots 
+using StatsPlots 
+
+# Analyzing ecological networks
+using EcologicalNetworks 
+
+
+## Load scripts
+include(joinpath("code", "01_clean_data.jl"))
+
