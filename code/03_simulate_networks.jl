849be532169d2bf1f91a2df5139e395ea1f5ebbc
CorinnePSoucy <142430060+CorinnePSoucy@users.noreply.github.com>
2025-11-30 23:25:31 -0500

    Update 03_simulate_networks.jl
    Code updated to remove Burgess and Chengjiang as those were not used in the final anayses.

--- code/03_simulate_networks.jl
+++ code/03_simulate_networks.jl
@@ -4,12 +4,6 @@
 N_fezouata = load(joinpath("data", "clean", "network_Fezouata.jld2"))["N"]
 N_fezouata_trophicsp = load(joinpath("data", "clean", "network_Fezouata_trophicsp.jld2"))["N"]
 
-N_burgess = load(joinpath("data", "clean", "network_burgess.jld2"))["N"]
-N_burgess_trophicsp = load(joinpath("data", "clean", "network_burgess_trophicsp.jld2"))["N"]
-
-N_chengjiang = load(joinpath("data", "clean", "network_chengjiang.jld2"))["N"]
-N_chengjiang_trophicsp = load(joinpath("data", "clean", "network_chengjiang_trophicsp.jld2"))["N"]
-
 
 # simulate networks using the niche, cascade, and nested-hierarchy models for each empirical network
 # n simulations for each model 
@@ -38,16 +32,8 @@ end
 Ns_fezouata_sim = simulate_networks(N_fezouata)
 Ns_fezouata_trophicsp_sim = simulate_networks(N_fezouata_trophicsp)
 
-Ns_burgess_sim = simulate_networks(N_burgess)
-Ns_burgess_trophicsp_sim = simulate_networks(N_burgess_trophicsp)
-
-Ns_chengjiang_sim = simulate_networks(N_chengjiang) 
-Ns_chengjiang_trophicsp_sim = simulate_networks(N_chengjiang_trophicsp)
 
 save(joinpath("data", "sim", "networks_fezouata_sim.jld2"), "Ns", Ns_fezouata_sim)
 save(joinpath("data", "sim", "networks_fezouata_trophicsp_sim.jld2"), "Ns", Ns_fezouata_trophicsp_sim)
-save(joinpath("data", "sim", "networks_burgess_sim.jld2"), "Ns", Ns_burgess_sim)
-save(joinpath("data", "sim", "networks_burgess_trophicsp_sim.jld2"), "Ns", Ns_burgess_trophicsp_sim)
-save(joinpath("data", "sim", "networks_chengjiang_sim.jld2"), "Ns", Ns_chengjiang_sim)
-save(joinpath("data", "sim", "networks_chengjiang_trophicsp_sim.jld2"), "Ns", Ns_chengjiang_trophicsp_sim)
+
 
